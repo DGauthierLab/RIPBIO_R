85d21c83b7e5c73ecc101c33426af7967fef6288
losteider <dgauthie@odu.edu>
2023-02-06 14:28:40 -0500

    first commit

---
+++ RIPBIO_R/utility_script.R
@@ -0,0 +1,58 @@
+####Initialize####
+library(ggplot2)
+library(tidyverse)
+####Distributions####
+x_dhyper <- seq(0, 4, by = 1)
+
+# Apply dhyper function
+y_dhyper <- dhyper(x_dhyper, m = 4, n = 4, k = 4)   
+
+# Plot dhyper values
+barplot(y_dhyper, ylab = "Probability", xlab = "Successes", names.arg = c(0,1,2,3,4)) 
+abline (h=0.05, col="red")
+
+#Create a sequence of 100 equally spaced numbers between -4 and 4
+x <- seq(-4, 4, length=100)
+
+#create a vector of values that shows the height of the probability distribution
+#for each value in x
+y <- dnorm(x)
+
+#plot x and y as a scatterplot with connected lines (type = "l") and add
+#an x-axis with custom labels
+plot(x,y, type = "l", lwd = 2, axes = FALSE, xlab = "", ylab = "")
+axis(1, at = -3:3, labels = c("-3s", "-2s", "-1s", "mean", "1s", "2s", "3s"))
+
+
+#generate normally distributed random numbers
+pop1=rnorm(100,20)
+pop2=rnorm(100,20)
+
+tickwts <- cbind(pop1,pop2)
+View(tickwts)
+tickwts <- as_tibble(tickwts)
+
+
+tickwts <- as_tibble(tickwts) %>% 
+  pivot_longer(
+    everything(),
+    names_to = "population",
+    values_to = "weight_mg"
+  ) %>%
+  arrange(population)
+
+plot <- tickwts %>% 
+  ggplot(aes(x=population,
+             y=weight)) +
+  geom_point(size = 3)
+
+boxplot <- tickwts %>% 
+  ggplot(aes(x=population,
+             y=weight)) +
+  geom_boxplot()
+
+histo <- tickwts %>% 
+  ggplot(aes(y=weight, color=population)) +
+  geom_histogram() +
+  coord_flip()
+histo
